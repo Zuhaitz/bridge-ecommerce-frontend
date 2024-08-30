c51b052633bc6eab22186996d0fbf616407f2c54
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-08-30 19:21:17 +0200

    home page done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import "./Home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home__message">
+        <p className="home__welcome">Welcome :)</p>
+        <p className="home__subtext">
+          buy stuff or i won't be able to feed my family
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
